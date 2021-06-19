d0602c31c7de8949e2e3763d660802018e5d60b8
NIcola R <nicola.rizzi89@hotmail.it>
2021-06-19 15:51:47 +0200

    toggle done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 /* MATERIAL */
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -20,7 +20,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSlideToggleModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule
 
   ],
   providers: [],
